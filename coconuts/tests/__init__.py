4c0812fab4197261c7daa26d9ce8ed549c8ff4d8
Jeremy Lainé <jeremy.laine@m4x.org>
2013-09-10 09:52:30 +0200

    add some unit tests

--- coconuts/tests/__init__.py
+++ coconuts/tests/__init__.py
@@ -41,17 +41,53 @@ class BaseTest(TestCase):
 class HomeTest(BaseTest):
     fixtures = ['test_users.json']
 
-    def test_home_anonymous(self):
+    def test_as_anonymous(self):
         """
         Anonymous users need to login.
         """
         response = self.client.get('/')
         self.assertRedirects(response, '/accounts/login/?next=/')
 
-    def test_home_superuser(self):
+    def test_as_superuser(self):
         """
         Authenticated super-user can browse the home folder.
         """
-        self.client.login(username="test", password="test")
+        self.client.login(username="test_user_1", password="test")
         response = self.client.get('/')
         self.assertContains(response, '<h1>Shares</h1>')
+
+    def test_as_user(self):
+        """
+        Authenticated user cannot browse the home folder.
+        """
+        self.client.login(username="test_user_2", password="test")
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 403)
+
+class AddFolderTest(BaseTest):
+    fixtures = ['test_users.json']
+
+    def test_as_superuser(self):
+        """
+        Authenticated super-user can create a folder.
+        """
+        self.client.login(username="test_user_1", password="test")
+
+        response = self.client.get('/images/add_folder/')
+        self.assertContains(response, 'Create a folder')
+
+        # create folder
+        response = self.client.post('/images/add_folder/', {'name': 'New folder'})
+        self.assertRedirects(response, '/')
+
+        # check folder
+        data_path = os.path.join(settings.COCONUTS_DATA_ROOT, 'New folder')
+        self.assertTrue(os.path.exists(data_path))
+
+    def test_as_user(self):
+        """
+        Authenticated user cannot create a folder.
+        """
+        self.client.login(username="test_user_2", password="test")
+        response = self.client.get('/images/add_folder/')
+        self.assertEquals(response.status_code, 403)
