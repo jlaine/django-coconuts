5a084c381d70f4cd400c0c49eab247431997c4fe
Jeremy Lainé <jeremy.laine@m4x.org>
2013-11-12 14:38:48 +0100

    re-add bundled EXIF.py

--- coconuts/views.py
+++ coconuts/views.py
@@ -16,7 +16,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import EXIF
+try:
+    import EXIF
+except:
+    from coconuts import EXIF
 import json
 import mimetypes
 import os
