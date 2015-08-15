91457c1c2c12b5b7eb43445b7766417348e42401
Jeremy Lainé <jeremy.laine@m4x.org>
2015-08-15 14:57:56 +0200

    improve coPhoto test

--- coconuts/static/test/unit/directiveSpec.js
+++ coconuts/static/test/unit/directiveSpec.js
@@ -30,7 +30,12 @@ describe('Directives', function() {
         });
 
         it('should handle defined photo', function() {
-            scope.some_photo = {mimetype: 'image/jpeg', name: 'bar.jpg', path: '/foo/bar.jpg'};
+            scope.some_photo = {
+                image: { width: 1024, height: 768 },
+                mimetype: 'image/jpeg',
+                name: 'bar.jpg',
+                path: '/foo/bar.jpg'
+            };
             scope.$digest();
             expect(elem.attr('alt')).toBe('bar.jpg');
             expect(elem.attr('src')).toBe('images/render/foo/bar.jpg?size=128');
