96d70eb672ff6d7500a6a28d3e28cd2e7c0ad25f
Jeremy Lainé <jeremy.laine@m4x.org>
2016-03-28 21:09:38 +0200

    fix getting video info when video stream is not the first

--- coconuts/views.py
+++ coconuts/views.py
@@ -187,12 +187,13 @@ def get_video_info(filepath):
     Gets a video's information.
     """
     data = json.loads(subprocess.check_output(['avprobe', '-of', 'json', '-loglevel', 'quiet', '-show_streams', '-show_format', filepath]))
-    stream = data['streams'][0]
-    return {
-        'duration': float(stream['duration']),
-        'height': stream['height'],
-        'width': stream['width'],
-    }
+    for stream in data['streams']:
+        if stream['codec_type'] == 'video':
+            return {
+                'duration': float(stream['duration']),
+                'height': stream['height'],
+                'width': stream['width'],
+            }
 
 def has_permission(path, perm, user):
     """
