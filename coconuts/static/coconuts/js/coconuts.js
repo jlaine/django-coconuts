c8ae03aca603a9612f0c992d264ef5519410f9c8
Jeremy Lainé <jeremy.laine@m4x.org>
2024-08-18 12:36:26 +0200

    Remove legacy fullscreen code

--- coconuts/static/coconuts/js/coconuts.js
+++ coconuts/static/coconuts/js/coconuts.js
@@ -48,23 +48,10 @@ controller('CrumbCtrl', ['$location', '$rootScope', '$scope', 'settings', functi
     });
 
     $scope.toggleFullScreen = function() {
-        if (!document.fullscreenElement &&    // alternative standard method
-            !document.mozFullScreenElement && !document.webkitFullscreenElement) {  // current working methods
-            if (document.documentElement.requestFullscreen) {
-                document.documentElement.requestFullscreen();
-            } else if (document.documentElement.mozRequestFullScreen) {
-                document.documentElement.mozRequestFullScreen();
-            } else if (document.documentElement.webkitRequestFullscreen) {
-                document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-            }
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen();
         } else {
-            if (document.cancelFullScreen) {
-                document.cancelFullScreen();
-            } else if (document.mozCancelFullScreen) {
-                document.mozCancelFullScreen();
-            } else if (document.webkitCancelFullScreen) {
-                document.webkitCancelFullScreen();
-            }
+            document.cancelFullScreen();
         }
     };
 }]).
