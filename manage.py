7591c5639dd9eb3ebdf09afe037cf7cae04c91c9
Jeremy Lainé <jeremy.laine@m4x.org>
2024-08-19 20:11:13 +0200

    Reformat code using `ruff`

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    if 'test' in sys.argv:
+    if "test" in sys.argv:
         os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
 
     from django.core.management import execute_from_command_line
