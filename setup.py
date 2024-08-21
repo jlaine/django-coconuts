e98982e93b2df6c098f12cb2b1bb88648aef9a09
Jeremy Lainé <jeremy.laine@m4x.org>
2024-08-21 18:50:00 +0200

    Cleanup package layout, prune redundant copyright
    We do not need copyright notices all over the place, we have a LICENSE
    file for that.

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-#!/usr/bin/env python
+import setuptools
 
-from distutils.core import setup
-
-setup(
-    name="coconuts",
-    version="0.3.0",
-    license="GPL",
-    url="https://github.com/jlaine/django-coconuts",
-    packages=["coconuts"],
-    package_data={
-        "coconuts": [
-            "static/coconuts/css/*.css",
-            "static/coconuts/img/*.png",
-            "static/coconuts/index.html",
-            "static/coconuts/js/*.js",
-            "templates/coconuts/*.html",
-        ]
-    },
-)
+setuptools.setup()
