a10485cf276e6fc3b31523f274d8cad8a19fa5ec
Jeremy Lainé <jeremy.laine@m4x.org>
2025-06-09 11:21:53 +0200

    Add tests for `RouterService`

--- frontend/src/app/router.service.spec.ts
+++ frontend/src/app/router.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { RouterService } from './router.service';
+import { RouterService, stripBasePath, stripTrailingSlash } from './router.service';
 
 describe('RouterService', () => {
     let service: RouterService;
@@ -10,7 +10,32 @@ describe('RouterService', () => {
         service = TestBed.inject(RouterService);
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
+    it('should navigate', () => {
+        const paths: string[] = [];
+        const spy = spyOn(window.history, 'pushState');
+        const sub = service.path$.subscribe((path) => {
+            paths.push(path);
+        });
+
+        service.pathPrefix = '/app/';
+        expect(paths).toEqual(['']);
+
+        service.go('some/path/');
+        expect(spy).toHaveBeenCalledWith(null, '', '/app/some/path/');
+        expect(paths).toEqual(['', 'some/path/']);
+
+        sub.unsubscribe();
+    });
+
+    it('should strip base path', () => {
+        expect(stripBasePath('/', '/some/path')).toBe('some/path');
+
+        expect(stripBasePath('/app/', '/app/some/path')).toBe('some/path');
+        expect(stripBasePath('/app/', '/some/path')).toBe('/some/path');
+    });
+
+    it('should strip trailing slash', () => {
+        expect(stripTrailingSlash('foo/bar')).toBe('foo/bar');
+        expect(stripTrailingSlash('foo/bar/')).toBe('foo/bar');
     });
 });
